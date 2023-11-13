736a6300f643fb8c7ce31e5a531265ed8a32c89a
Sarkies <Ahmed.Sarkes@gmail.com>
2023-11-13 21:59:41 +0200

    update by env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBA7ZeQCogWvpmKAghqj2wgFyoK9BS1i_w",
-  authDomain: "maltimarket-bf62d.firebaseapp.com",
-  projectId: "maltimarket-bf62d",
-  storageBucket: "maltimarket-bf62d.appspot.com",
-  messagingSenderId: "829658027881",
-  appId: "1:829658027881:web:45e0eb0ad0c4ab0aa38ff8",
-  measurementId: "G-J36MLJ5GVZ"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
