bcdb6bcdbf0bafc667a7aefeed6debb979ab1040
Sarkies <Ahmed.Sarkes@gmail.com>
2023-11-13 22:03:19 +0200

    update Layout Components

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,10 +3,15 @@ import React, { Fragment } from "react";
 import { Header, Footer } from "../../components";
 import Routers from "../../routers/Routers";
 
+import { AdminNav } from "../../admin";
+import { useLocation } from "react-router-dom";
+
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <Fragment>
-      <Header />
+      {location.pathname.includes("dashboard") ? <AdminNav /> : <Header />}
       <div>
         <Routers />
       </div>
