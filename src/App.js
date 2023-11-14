655666ce8d7d49ef0c4097aa1348250f079696c3
Sarkies <Ahmed.Sarkes@gmail.com>
2023-11-14 13:20:33 +0200

    Update App.js and make it more protected and improve user experience

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useMemo, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { Layout } from "./components";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (location.pathname.endsWith("/")) {
+      for (let i = location.pathname.length - 1; i >= 0; i--) {
+        if (location.pathname[i] === "/") {
+          navigate(location.pathname.slice(0, i));
+          break;
+        }
+      }
+    }
+  }, [location.pathname, navigate]);
   return <Layout />;
 }
 
