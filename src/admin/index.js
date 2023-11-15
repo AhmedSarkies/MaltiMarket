0ff02a70c42bef2146e0d1b502c1723a2a9f58e1
Sarkies <Ahmed.Sarkes@gmail.com>
2023-11-15 20:05:56 +0200

    New Updates in index admin

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,5 +2,5 @@ export { default as Dashboard } from "./Dashboard";
 export { default as AdminNav } from "./AdminNav";
 export { default as AddProduct } from "./AddProduct";
 export { default as AllProducts } from "./AllProducts";
-export { default as AdminOrders } from "./AdminOrders";
-export { default as AdminUsers } from "./AdminUsers";
+export { default as Orders } from "./Orders";
+export { default as Users } from "./Users";
