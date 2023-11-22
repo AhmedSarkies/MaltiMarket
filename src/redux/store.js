a58ce56ad87dbc74fa96906f7d1f5ed27dbf126f
Sarkies <Ahmed.Sarkes@gmail.com>
2023-11-22 23:23:56 +0200

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import cartSlice from "./slices/cartSlice";
+import authSlice from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
+    auth: authSlice,
   },
 });
 
